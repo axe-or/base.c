b9051f5bc40eca33d1d8ca1c58593bec2df8e3a1
marcs-feh <marcosfehlauer@protonmail.com>
2024-09-08 20:16:10 -0300

    add license and bundle with version

--- bundle.py
+++ bundle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import zipfile as zf
+import subprocess as sb
+from os import remove
 
 files = [
     'arena_allocator.h',
@@ -17,6 +19,12 @@ files = [
 
 OUT_ARCHIVE = 'libc2.zip'
 OUT_SRC = 'libc2.c'
+
+VERSION_FILE = 'version.txt'
+revision = sb.check_output(["git", "rev-parse", "--short=16", "HEAD"]).decode('utf-8')
+with open(VERSION_FILE, 'w') as f:
+    f.write(revision + '\n')
+
 with open(OUT_SRC, 'w') as f:
     source =  ["/* Convenience file to have a translation unit that implements all of libc2's utilities. */"]
     source += ['#define LIBC2_IMPLEMENTATION 1']
@@ -25,18 +33,16 @@ with open(OUT_SRC, 'w') as f:
     n = f.write('\n'.join(source) + '\n')
     print(f'Wrote {n}B to {OUT_SRC}')
 
-
 ROOT = 'libc2'
-include_dir = f'{ROOT}/include'
-src_dir = f'{ROOT}/src'
 
-with zf.ZipFile(OUT_ARCHIVE, 'w', compression=zf.ZIP_LZMA) as zfile:
+with zf.ZipFile(OUT_ARCHIVE, 'w', compression=zf.ZIP_LZMA, compresslevel=9) as zfile:
     zfile.mkdir(ROOT)
-    zfile.mkdir(include_dir)
-    zfile.mkdir(src_dir)
     for f in files:
-        zfile.write(f, f'{include_dir}/{f}')
+        zfile.write(f, f'{ROOT}/{f}')
 
-    zfile.write(OUT_SRC, f'{src_dir}/{OUT_SRC}')
+    zfile.write(OUT_SRC, f'{ROOT}/{OUT_SRC}')
+    zfile.write('LICENSE', f'{ROOT}/LICENSE')
+    zfile.write('version.txt', f'{ROOT}/version.txt')
+    remove(VERSION_FILE)
     print(f'Created {OUT_ARCHIVE}')
 
