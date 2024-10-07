9b3d1ecb09cdac780916343f3bc9ca8dbef95424
marcs-feh <marcosfehlauer@protonmail.com>
2024-10-06 23:16:29 -0300

    cleanup

--- base/heap_allocator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "memory.h"
-
-// Get the system's heap allocator, this will typically be LibC's but could also
-// be a custom one or a 3rd party one like mimalloc.
-Mem_Allocator heap_allocator();
-
