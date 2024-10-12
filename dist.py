225a1f6332350c4e1f4bdab7d57306c8de0e22d2
marcs-feh <marcosfehlauer@protonmail.com>
2024-10-12 18:20:47 -0300

    2-file yeeee hah

--- dist.py
+++ dist.py
@@ -2,30 +2,65 @@ from os import listdir, path, mkdir
 from shutil import copy as file_copy
 from zipfile import ZIP_DEFLATED, ZipFile
 
+def remove_includes(data: str, rem_set: list[str]):
+    for rm in rem_set:
+        data = data.replace(f'#include "{rm}"', f'/* #include "{rm}" */')
+        data = data.replace(f'#include "./{rm}"', f'/* #include "./{rm}" */')
+    return data
+
 def main():
     sources = glob_extension('src', '.c', exclude=['main.c', 'base.c'])
-    headers = glob_extension('src', '.h')
-
-    data = ['/* Single compilation unit for whole library */']
-    sources.sort()
-    for src in sources:
-        with open(src, 'r') as f:
-            data.append(f'/* File: {src} */')
-            data.append(f.read())
-    data = '\n'.join(data) + '\n'
+    headers = [
+        # Foundations (Freestanding headers)
+        'prelude.h',
+        'context.h',
+        'spinlock.h',
+        # Important definitions
+        'assert.h',
+        'io.h',
+        'memory.h',
+        'list.h',
+        'utf8.h',
+        # General utilities and platform-agnostic implementations
+        'string.h',
+        'bytes_buffer.h',
+        'arena_allocator.h',
+        'fnv_hash.h',
+        'command_line_opt.h',
+        # Platform specific implementation
+        'heap_allocator.h',
+    ]
 
     DIR = 'base'
-    try:
-        mkdir(DIR)
-    except FileExistsError:
-        pass
+    try: mkdir(DIR)
+    except FileExistsError: pass
 
-    with open(path.join(DIR, 'base.c'), 'w') as f:
-        n = f.write(data)
+    # Create .h amalagamation
+    with open(path.join(DIR, 'base.h'), 'w') as amalgam:
+        data = ['#pragma once']
+        for header in headers:
+            with open(path.join('src', header), 'r') as f:
+                data.append(f'/* File: {header} */')
+                code = remove_includes(f.read(), headers)
+                data.append(code.replace('#pragma once', ''))
+            data.append('')
+        n = amalgam.write('\n'.join(data))
+        print(f'Wrote {n}B to base.h')
+
+    # Create .c amalagamation
+    with open(path.join(DIR, 'base.c'), 'w') as amalgam:
+        data = ['#include "./base.h"']
+        sources.sort()
+        for src in sources:
+            with open(src, 'r') as f:
+                data.append(f'/* File: {src} */')
+                code = remove_includes(f.read(), headers)
+                data.append(code)
+        data.append('')
+
+        n = amalgam.write('\n'.join(data))
         print(f'Wrote {n}B to base.c')
 
-    for header in headers:
-        file_copy(header, DIR)
     file_copy('LICENSE', DIR)
 
     with ZipFile('base.zip', 'w', compression=ZIP_DEFLATED, compresslevel=9) as zf:
--- main.c
+++ main.c
@@ -1,15 +1,5 @@
-#define BASE_C_IMPLEMENTATION 1
-#include "prelude.h"
-#include "heap_allocator.h"
-#include "command_line_opt.h"
-#include "string.h"
+#include "base.h"
 #include <stdio.h>
-
-#include "context.h"
-// #define container_name Int_Array
-// #define container_type_param i64
-// #define container_prefix ia
-// #include "generic/dynamic_array.h"
 
 int main(int argc, char** argv){
 	printf(
